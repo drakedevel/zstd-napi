653bed36e048320ad69ddbd90285994a3ce30af2
Andrew Drake <adrake@adrake.org>
2020-01-26 15:58:00 -0500

    Add getDictID_fromDict/fromFrame.

--- src/binding.cc
+++ src/binding.cc
@@ -9,11 +9,37 @@
 
 using namespace Napi;
 
+// Dictionary helper functions
+Value wrapGetDictIDFromDict(const CallbackInfo& info) {
+  Env env = info.Env();
+  if (info.Length() != 1)
+    throw TypeError::New(env, "Wrong arguments");
+  Buffer<char> dictBuf = info[0].As<Buffer<char>>();
+  return Number::New(
+      env, ZSTD_getDictID_fromDict(dictBuf.Data(), dictBuf.ByteLength()));
+}
+
+Value wrapGetDictIDFromFrame(const CallbackInfo& info) {
+  Env env = info.Env();
+  if (info.Length() != 1)
+    throw TypeError::New(env, "Wrong arguments");
+  Buffer<char> frameBuf = info[0].As<Buffer<char>>();
+  return Number::New(
+      env, ZSTD_getDictID_fromFrame(frameBuf.Data(), frameBuf.ByteLength()));
+}
+
 Object ModuleInit(Env env, Object exports) {
   CCtx::Init(env, exports);
   CDict::Init(env, exports);
   DCtx::Init(env, exports);
   DDict::Init(env, exports);
+
+  exports.DefineProperties({
+      PropertyDescriptor::Function(env, exports, "getDictIDFromDict",
+                                   wrapGetDictIDFromDict),
+      PropertyDescriptor::Function(env, exports, "getDictIDFromFrame",
+                                   wrapGetDictIDFromFrame),
+  });
 
   return exports;
 }
