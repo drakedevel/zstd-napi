bc9c0200d4d6a5a41465749475c2755a1db3c842
Andrew Drake <adrake@adrake.org>
2020-01-29 00:59:14 -0500

    Add several simple top-level functions.

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const buildType = process.config.target_defaults ?
 const bindingPath = require.resolve(`./build/${buildType}/binding`);
 const binding = require(bindingPath);
 
+console.log(binding.versionNumber(), binding.versionString());
+console.log(binding.minCLevel(), binding.maxCLevel());
+
 const ctx = new binding.CCtx();
 const inBuf = fs.readFileSync('dummy-data/macbeth-300');
-const outBuf = Buffer.alloc(inBuf.length * 2);
+const outBuf = Buffer.alloc(binding.compressBound(inBuf.length));
 
 const cdict = new binding.CDict(fs.readFileSync('dummy-data-4k.bin'), 3);
 
@@ -20,3 +23,7 @@ if (len > 0) {
   fs.writeFileSync('out.bin', outBuf.slice(0, len));
 }
 console.log((endT - startT));
+
+console.log(len, binding.findFrameCompressedSize(outBuf));
+console.log(binding.getFrameContentSize(outBuf.slice(0, len)));
+console.log(binding.getFrameContentSize(fs.readFileSync('noframe.zst')));
--- src/binding.cc
+++ src/binding.cc
@@ -6,8 +6,62 @@
 #include "cdict.h"
 #include "dctx.h"
 #include "ddict.h"
+#include "zstd_util.h"
 
 using namespace Napi;
+
+// Version functions
+Value wrapVersionNumber(const CallbackInfo& info) {
+  return Number::New(info.Env(), ZSTD_versionNumber());
+}
+
+Value wrapVersionString(const CallbackInfo& info) {
+  return String::New(info.Env(), ZSTD_versionString());
+}
+
+// Simple API
+Value wrapGetFrameContentSize(const CallbackInfo& info) {
+  Env env = info.Env();
+  if (info.Length() != 1)
+    throw TypeError::New(env, "Wrong arguments");
+  Buffer<char> frameBuf = info[0].As<Buffer<char>>();
+
+  unsigned long long size =
+      ZSTD_getFrameContentSize(frameBuf.Data(), frameBuf.ByteLength());
+  if (size == ZSTD_CONTENTSIZE_UNKNOWN)
+    return env.Null();
+  if (size == ZSTD_CONTENTSIZE_ERROR)
+    throw Error::New(env, "Could not parse Zstandard header");
+  return Number::New(env, size);
+}
+
+Value wrapFindFrameCompressedSize(const CallbackInfo& info) {
+  Env env = info.Env();
+  if (info.Length() != 1)
+    throw TypeError::New(env, "Wrong arguments");
+  Buffer<char> frameBuf = info[0].As<Buffer<char>>();
+
+  return convertZstdResult(env, ZSTD_findFrameCompressedSize(
+                                    frameBuf.Data(), frameBuf.ByteLength()));
+}
+
+// Helper functions
+Value wrapCompressBound(const CallbackInfo& info) {
+  Env env = info.Env();
+  if (info.Length() != 1)
+    throw TypeError::New(env, "Wrong arguments");
+  int64_t size = info[0].ToNumber().As<Number>();
+
+  return Number::New(env, ZSTD_compressBound(size));
+}
+
+Value wrapMinCLevel(const CallbackInfo& info) {
+  return Number::New(info.Env(), ZSTD_minCLevel());
+}
+
+Value wrapMaxCLevel(const CallbackInfo& info) {
+  return Number::New(info.Env(), ZSTD_maxCLevel());
+}
 
 // Dictionary helper functions
 Value wrapGetDictIDFromDict(const CallbackInfo& info) {
@@ -35,6 +89,18 @@ Object ModuleInit(Env env, Object exports) {
   DDict::Init(env, exports);
 
   exports.DefineProperties({
+      PropertyDescriptor::Function(env, exports, "versionNumber",
+                                   wrapVersionNumber),
+      PropertyDescriptor::Function(env, exports, "versionString",
+                                   wrapVersionString),
+      PropertyDescriptor::Function(env, exports, "getFrameContentSize",
+                                   wrapGetFrameContentSize),
+      PropertyDescriptor::Function(env, exports, "findFrameCompressedSize",
+                                   wrapFindFrameCompressedSize),
+      PropertyDescriptor::Function(env, exports, "compressBound",
+                                   wrapCompressBound),
+      PropertyDescriptor::Function(env, exports, "minCLevel", wrapMinCLevel),
+      PropertyDescriptor::Function(env, exports, "maxCLevel", wrapMaxCLevel),
       PropertyDescriptor::Function(env, exports, "getDictIDFromDict",
                                    wrapGetDictIDFromDict),
       PropertyDescriptor::Function(env, exports, "getDictIDFromFrame",
