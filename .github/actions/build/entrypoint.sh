b5fb282a89071c68217d9c313327b1cfcce49caa
Andrew Drake <adrake@adrake.org>
2024-01-01 20:00:22 -0800

    Replace prebuild with a very small shell script.

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -3,4 +3,4 @@ set -eu
 npm ci --ignore-scripts
 # CentOS 7 is too old to support -std=gnu++14
 export ZSTD_NAPI_DISABLE_CPP14=1
-npm run build-prebuild
+./.github/prebuild.sh
---
+++ .github/prebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu -o pipefail
+cd "$(dirname "$0")/.."
+
+mkdir -p prebuilds
+platform="$(node -p process.platform)"
+arch="${1:-$(node -p process.arch)}"
+template="$(jq -r '"\(.name)-v\(.version)"' package.json)-napi-vNAPI_VER-${platform}-${arch}.tar.gz"
+for napi_ver in $(jq .binary.napi_versions[] package.json); do
+  ./node_modules/.bin/node-gyp rebuild --napi_build_version="$napi_ver" --target_arch="$arch"
+  tar c --format pax --numeric-owner build/Release/*.node |
+    gzip -9 -n > "prebuilds/${template/NAPI_VER/${napi_ver}}"
+done
