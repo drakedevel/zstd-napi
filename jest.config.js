171b94e085af33bdee4a34b658b31874a468961a
Andrew Drake <adrake@adrake.org>
2025-05-10 22:13:44 -0700

    Use new ts-jest configuration preset mechanism

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,8 @@
+const { createDefaultPreset } = require('ts-jest');
 module.exports = {
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.ts$': [
-      'ts-jest',
-      {
-        // Suppress warnings abut esModuleInterop, which is disabled per semver-ts
-        diagnostics: { ignoreCodes: ['TS151001'] },
-      },
-    ],
-  },
+  ...createDefaultPreset(),
   collectCoverageFrom: ['binding.js', 'lib/**/*.{js,ts}'],
   // TODO: Remove after upgrading to Jest 30
   prettierPath: null,
-  testEnvironment: 'node',
   testMatch: ['**/tests/**/*.ts'],
 };
